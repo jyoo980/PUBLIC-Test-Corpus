e22823354306bc0cba39021cc24aa3f8ee140951
James Yoo <jyoo980@gmail.com>
2026-05-13 09:21:50 -0700

    Adding annos written by Avocado Agent

--- Public-Tests/B01_organic/bin2hex_lib/test_case/src/lib.c
+++ Public-Tests/B01_organic/bin2hex_lib/test_case/src/lib.c
@@ -4,7 +4,15 @@
 #include "lib.h"
 
 char *bin2hex(char *hex, size_t hex_maxlen, const uint8_t *bin,
-                    size_t bin_len) {
+                    size_t bin_len)
+__CPROVER_requires(bin_len < 4)
+__CPROVER_requires(hex_maxlen > bin_len * 2U)
+__CPROVER_requires(hex_maxlen < 16)
+__CPROVER_requires(__CPROVER_is_fresh(hex, hex_maxlen))
+__CPROVER_requires((bin_len > 0) ==> __CPROVER_is_fresh(bin, bin_len))
+__CPROVER_assigns(__CPROVER_object_upto(hex, bin_len * 2U + 1U))
+__CPROVER_ensures(__CPROVER_return_value == hex)
+{
     size_t i = (size_t)0U;
     unsigned int x;
     int b;
